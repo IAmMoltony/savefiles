0be558a2e8e82d714bdf36391c4e726221833fc3
IAmMoltony <koronavirusnyj@gmail.com>
2024-06-10 21:29:46 +0500

    fix autobackup script

--- autobackup.sh
+++ autobackup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 
 echo "[Auto] Starting automatic backup"
+cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ./backup.py backup
 git add saves
 git commit -m "Automatic backup: $(date +"%F %T")"
