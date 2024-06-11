920b7dc858a6602cb66dc9b1aac586b67906bef1
IAmMoltony <koronavirusnyj@gmail.com>
2024-06-11 11:50:04 +0500

    Add ignore worlds in Minecraft backupper

--- backupper_minecraft.py
+++ backupper_minecraft.py
@@ -1,9 +1,19 @@
-from backupper import Backupper
+from backupper import Backupper, BackupperError
+import os
 
 
 class MinecraftBackupper(Backupper):
-    def __init__(self, paths: dict[str], machine_name: str):
-        super().__init__(paths, machine_name, "Minecraft")
+    def __init__(self, paths: dict[str], machine_name: str, config: dict):
+        super().__init__(paths, machine_name, "Minecraft", config)
 
     def backup(self):
-        self.copydir("saves")
+        saves = self.listdir("saves")
+        saves_clean = []
+        for save in saves:
+            if save in self.config["IgnoreWorlds"]:
+                print(f"[{self.game_name}] World '{save}' ignored")
+            else:
+                saves_clean.append(save)
+
+        for save in saves_clean:
+            self.copydir(os.path.join("saves", save))
