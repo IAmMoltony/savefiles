3fc63ec5e7e3b5617b23892aa35936c62040602c
IAmMoltony <koronavirusnyj@gmail.com>
2024-06-11 11:23:04 +0500

    Add disabling/enabling autobackup

--- autobackup.sh
+++ autobackup.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/bash
 
+if [ ! -z "$1" ]; then
+    echo "[Auto] Argument detected"
+    if [ "$1" == "ENABLE" ]; then
+        echo "[Auto] Enabling autobackup"
+        rm -f ~/.savefilesdisabled
+    elif [ "$1" == "DISABLE" ]; then
+        echo "[Auto] Disabling autobackup"
+        touch ~/.savefilesdisabled
+    else
+        echo "[Auto] Unknown argument $1"
+    fi
+
+    exit
+fi
+
+if [ -f "$HOME/.savefilesdisabled" ]; then
+    echo "[Auto] Autobackup is disabled"
+    exit
+fi
+
 echo "[Auto] Starting automatic backup"
 cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ./backup.py backup
