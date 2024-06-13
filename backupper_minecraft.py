a0cd8feb2d29f48a56db9ad43c8bdf01bf6fa5ca
IAmMoltony <koronavirusnyj@gmail.com>
2024-06-13 12:11:21 +0500

    Fix minecraft backupper crashing if IgnoreWorlds not found in config

--- backupper_minecraft.py
+++ backupper_minecraft.py
@@ -7,10 +7,14 @@ class MinecraftBackupper(Backupper):
         super().__init__(paths, machine_name, "Minecraft", config)
 
     def backup(self):
+        ignore_worlds = []
+        if "IgnoreWorlds" in self.config:
+            ignore_worlds = self.config["IgnoreWorlds"]
+
         saves = self.listdir("saves")
         saves_clean = []
         for save in saves:
-            if save in self.config["IgnoreWorlds"]:
+            if save in ignore_worlds:
                 print(f"[{self.game_name}] World '{save}' ignored")
             else:
                 saves_clean.append(save)
