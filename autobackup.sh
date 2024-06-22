89d8d4f0c65eb011e3a584b6853e7cf245181cff
IAmMoltony <koronavirusnyj@gmail.com>
2024-06-22 22:52:31 +0500

    Add one singular newline to autobackup

--- autobackup.sh
+++ autobackup.sh
@@ -22,6 +22,7 @@ fi
 
 echo "[Auto] Starting automatic backup"
 cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 ./backup.py backup
 git add saves
 git commit -m "Automatic backup: $(date +"%F %T")"
